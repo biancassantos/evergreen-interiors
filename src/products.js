7b988e499bedf91b2a3d542ad5eef1cf1cea4657
Bianca Santos <biancassantos89@gmail.com>
2024-08-16 13:30:57 -0300

    add descriptions

--- src/products.js
+++ src/products.js
@@ -13,7 +13,7 @@ const products = [
             'Simple Design: Uses fewer materials and processes.'
         ],
         productSpecifications: [
-            'Dimensions: 40cm x 40cm x 40cm',
+            'Dimensions: 40cm x 40cm x 60cm',
             'Material: Bamboo'
         ]
     }, {
@@ -21,57 +21,120 @@ const products = [
         productName: "Bed One",
         productImg: 'productsImg/bed-1.png',
         productPrice: 299,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Bed One is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 180cm x 120cm x 40cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 3,
         productName: "Bed Two",
         productImg: 'productsImg/bed-2.png',
         productPrice: 499,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Bed Two is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 180cm x 120cm x 40cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 4,
         productName: "Cupboard",
         productImg: 'productsImg/cupboard.png',
         productPrice: 159,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Cupboard is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 40cm x 60cm x 65cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 5,
         productName: "Bed Three",
         productImg: 'productsImg/bed-3.png',
         productPrice: 299,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Bed Three is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 180cm x 120cm x 40cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 6,
         productName: "Nightstand",
         productImg: 'productsImg/nightstand.png',
         productPrice: 99,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Nightstand is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 40cm x 50cm x 60cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 7,
         productName: "Armoire",
         productImg: 'productsImg/armoire.png',
         productPrice: 499,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Armoire is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 50cm x 100cm x 130cm',
+            'Material: Bamboo'
+        ]
     }, {
         id: 8,
         productName: "Lounge",
         productImg: 'productsImg/lounge.png',
         productPrice: 239,
-        productDescription: '',
-        descriptionList: [],
-        productSpecifications: []
+        productDescription: 'The Lounge is a piece of furniture designed with environmental consciousness and longevity in mind, not only meeting the functional needs but also aligning with eco-friendly practices, promoting a healthier planet and conscious consumerism. Here are some key characteristics:',
+        descriptionList: [
+            'Recycled or Upcycled Wood: Using reclaimed wood from old furniture or buildings.',
+            'Bamboo: A fast-growing and renewable resource.',
+            'Natural Oils and Waxes: Such as linseed oil or beeswax.',
+            'Timeless Aesthetics: Classic designs that won&apos;t go out of style.',
+            'Simple Design: Uses fewer materials and processes.'
+        ],
+        productSpecifications: [
+            'Dimensions: 50cm x 120cm x 40cm',
+            'Material: Bamboo'
+        ]
     }
 ];
 
