911ecdab89387735cfa9d6764e41fc33765a92f3
Bianca Santos <biancassantos89@gmail.com>
2024-08-16 12:50:27 -0300

    links and menu fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart, FaBars } from "react-icons/fa";
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+
   return (
     <header>
         <div className="header-content">
@@ -18,16 +21,22 @@ const Header = () => {
                         <FaShoppingCart />
                     </button>
                 </div>
-                <button className="menu-mobile-btn">
+                <button className="menu-mobile-btn" onClick={() => setOpen(!open)}>
                     <FaBars />
                 </button>
             </nav>
 
-            <nav className="mobile-menu">
+            <nav className={`mobile-menu ${open ? 'open-mobile-menu' : ''}`}>
                 <ul>
-                    <li className="menu-link"><Link to="/">home</Link></li>
-                    <li className="menu-link"><Link to="#">about</Link></li>
-                    <li className="menu-link"><Link to="#">my purchases</Link></li>
+                    <li className="menu-link" onClick={() => setOpen(false)}>
+                        <Link to="/evergreen-interiors">home</Link>
+                    </li>
+                    <li className="menu-link" onClick={() => setOpen(false)}>
+                        <Link to="#">about</Link>
+                    </li>
+                    <li className="menu-link" onClick={() => setOpen(false)}>
+                        <Link to="#">my purchases</Link>
+                    </li>
                 </ul>
             </nav>
         </div>
