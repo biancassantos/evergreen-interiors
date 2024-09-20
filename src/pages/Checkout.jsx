9988fee5580d8ff52c693af66fb35152e57221e9
Bianca Santos <biancassantos89@gmail.com>
2024-09-20 13:28:25 -0300

    checkout page fix

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import { CartContext } from "../context/CartContext";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import CheckoutCart from "../components/ui/CheckoutCart";
 import CheckoutTotal from "../components/ui/CheckoutTotal";
+import Message from "../components/ui/Message";
 
 const Checkout = () => {
+  const {cartQuantity} = useContext(CartContext);
+
   useEffect(() => {
     document.title = "Checkout"
   }, []);
@@ -14,8 +18,13 @@ const Checkout = () => {
       <Header />
       <main>
         <section className="checkout-body">
-          <CheckoutCart />
-          <CheckoutTotal />
+          {cartQuantity > 0 ? 
+          (<>
+            <CheckoutCart />
+            <CheckoutTotal />
+          </>) : 
+          (<Message msg="Your cart is empty." />)}
+          
         </section>
       </main>
       <Footer />
